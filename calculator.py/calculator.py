a7b76b1f5abb36b32bba12cff0fd644005ff2575
Ritik <ritikbhoyar133@gmail.com>
2025-09-01 09:27:07 +0530

    feature/sub

--- calculator.py/calculator.py
+++ calculator.py/calculator.py
@@ -1,9 +1,19 @@
 def add(a:int,b:int):
     return a + b
+def subtract(a:int,b:int):
+    return a - b
 try:
-    a = int(input("Enter the first number"))
-    b = int(input("Enter the second number"))
-    print(add(a,b))
-except ValueError:
-    print("Entered Invalid input , Only Intergers Allowed")
+    operation = input("Enter the operation you want to perform (add/subtract): ").strip().lower()
+    if operation not in ['add', 'subtract']:
+        raise ValueError("Invalid operation. Please enter 'add' or 'subtract'.")  
+    if operation == 'add':
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+        print(add(a,b))
+    elif operation == 'subtract':
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+        print(subtract(a,b))
+except ValueError as e:
+    print(f"Error: {e}")
 
